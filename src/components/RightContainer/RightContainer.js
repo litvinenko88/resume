04b7858e14b13cb98869aee199e765ab902b9464
Nikolay Litvinenko <Litvinenko_n.v@mail.ru>
2023-07-27 18:59:14 +0300

    commit

--- src/components/RightContainer/RightContainer.js
+++ src/components/RightContainer/RightContainer.js
@@ -1,10 +1,8 @@
-import NavSatet from "../../context/NavState";
 import Contacts from "./Contacts/Contacts";
 import Footer from "./Footer/Footer";
 import styles from "./RightContainer.module.css";
 import Skills from "./Skills/Skills";
 import React from "react";
-import MainMenu from "../Hamburger/MainMenu";
 
 function RightContainer() {
   return (
